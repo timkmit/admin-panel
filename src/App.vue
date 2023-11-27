9100a744ebac4e1a594300288dc514b8fcbb85ec
Timur <timengalytchev@mail.ru>
2023-11-27 15:05:51 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-layout view="1Hh Lpr lFf" class="bg-grey-2 text-dark">
+    <SideBar />
+    <q-page-container>
+      <router-view></router-view>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import SideBar from './components/SideBar.vue'
+  export default {
+
+  }
+</script>
