4b630bb306a255e8c5af739d058437abe072c3af
Timur <timengalytchev@mail.ru>
2023-11-28 03:15:05 +0300

    made a section with avatar of user on main page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <q-layout view="1Hh Lpr lFf" class="bg-grey-2 text-dark">
     <SideBar />
+    
     <q-page-container>
+      <Home/>
       <router-view></router-view>
     </q-page-container>
+    
   </q-layout>
 </template>
 
 <script>
 import SideBar from './components/SideBar.vue'
+import Home from './views/Home.vue';
   export default {
-    components: {SideBar}
+    components: { SideBar, Home }
   }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="q-pa-md">
-
+        <div class="row q-col-glutter-md">
+            <div class="col-4">
+            <q-card class="my-card q-py-sm">
+                <q-item-section avatar>
+                    <q-avatar square>
+                        <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+                    </q-avatar>
+                </q-item-section>
+            </q-card>
+        </div>
+        </div>
+        
     </div>
 </template>
 
