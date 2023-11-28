2efa48c2f17e6e05277a709c68ebaee77ba99714
Timur <timengalytchev@mail.ru>
2023-11-28 03:02:31 +0300

    made: sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -53,7 +53,89 @@
                     <q-item-label>Dashboard</q-item-label>
                 </q-item-section>
                 </q-item>
+
+
+                <q-item clickable
+                active-class="my-menu-link"
+                v-ripple
+                :active="link === 'Edit Page'"
+                @click="link = 'Edit Page'">
+
+                <q-item-section avatar>
+                    <q-icon name="fas fa-pencil-alt" />
+                </q-item-section>
+                <q-item-section>
+                    <q-item-label>Edit Page</q-item-label>
+                </q-item-section>
+                </q-item>
+
+
+                <q-item clickable
+                active-class="my-menu-link"
+                v-ripple
+                :active="link === 'Chat'"
+                @click="link = 'Chat'">
+
+                <q-item-section avatar>
+                    <q-icon name="fas fa-comments" />
+                </q-item-section>
+                <q-item-section>
+                    <q-item-label>Chat</q-item-label>
+                </q-item-section>
+                </q-item>
+
+
+                <q-item clickable
+                active-class="my-menu-link"
+                v-ripple
+                :active="link === 'Community'"
+                @click="link = 'Community'">
+
+                <q-item-section avatar>
+                    <q-icon name="fas fa-satellite-dish" />
+                </q-item-section>
+                <q-item-section>
+                    <q-item-label>Community</q-item-label>
+                </q-item-section>
+                </q-item>
+
+
+                <q-item clickable
+                active-class="my-menu-link"
+                v-ripple
+                :active="link === 'Settings'"
+                @click="link = 'Settings'">
+
+                <q-item-section avatar>
+                    <q-icon name="fas fa-cog" />
+                </q-item-section>
+                <q-item-section>
+                    <q-item-label>Settings</q-item-label>
+                </q-item-section>
+                </q-item>
+
+
+                <q-item clickable
+                active-class="my-menu-link"
+                v-ripple
+                :active="link === 'Creators'"
+                @click="link = 'Creators'">
+
+                <q-item-section avatar>
+                    <q-icon name="fas fa-user" />
+                </q-item-section>
+                <q-item-section>
+                    <q-item-label>Creators</q-item-label>
+                </q-item-section>
+                </q-item>
+
             </q-list>
+
+            <q-space></q-space>
+            <div class="q-py-md" style="position:absolute; bottom:0; margin-left:75px">
+                <q-btn color="light-blue-8" label="Logout" class="q-px-lg" outline />
+            </div>
+
         </q-drawer>
     </nav>
 </template>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="q-pa-md">
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Home',
+    data: () => ({
+        value:0
+    })
+}
+
+</script>
